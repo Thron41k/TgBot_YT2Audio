929158e882c27b0dc3752cbbc97d2d5273322a15
Timothy <Thron41k@gmail.com>
2024-09-30 12:16:32 +0800

    work in progress

--- TgBot_YT2Audio/Program.cs
+++ TgBot_YT2Audio/Program.cs
@@ -1,8 +1,20 @@
 ﻿using Telegram.Bot;
 using Telegram.Bot.Types;
 using TgBot_YT2Audio;
+using File = System.IO.File;
 
 
+if (args.Length == 0 & !File.Exists("token.json"))
+{
+    Console.WriteLine("Не обнаружен токен для авторизации бота.");
+    Console.WriteLine("Создайте в корне файл \"token.json\" с токеном или передайте в качестве первого аргумента путь к файлу с токеном.");
+    Console.WriteLine("Файл \"token.json\" должен иметь следующую структуру:");
+    Console.WriteLine(Environment.NewLine);
+    Console.WriteLine("{");
+    Console.WriteLine("\t\"Token\": \"ВАШ_ТОКЕН\"");
+    Console.WriteLine("}");
+    return;
+}
 Console.WriteLine(args[0]);
 using var cts = new CancellationTokenSource();
 var token = (await JsonFileReader.ReadAsync<JsonFileReader.TokenFile>("token.json"))?.Token;

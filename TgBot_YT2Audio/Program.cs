0bb25ab0582b4b12262fdd1c22b37be0c2bad051
Timothy <Thron41k@gmail.com>
2024-09-30 13:11:16 +0800

    work in progress

--- TgBot_YT2Audio/Program.cs
+++ TgBot_YT2Audio/Program.cs
@@ -1,10 +1,12 @@
-﻿using Telegram.Bot;
+﻿using System.Runtime.CompilerServices;
+using Telegram.Bot;
 using Telegram.Bot.Types;
 using TgBot_YT2Audio;
 using File = System.IO.File;
 
 
-if (args.Length == 0 & !File.Exists("token.json"))
+var token = TokenFileLoad();
+if (string.IsNullOrEmpty(token))
 {
     Console.WriteLine("Не обнаружен токен для авторизации бота.");
     Console.WriteLine("Создайте в корне файл \"token.json\" с токеном или передайте в качестве первого аргумента путь к файлу с токеном.");
@@ -15,27 +17,33 @@ if (args.Length == 0 & !File.Exists("token.json"))
     Console.WriteLine("}");
     return;
 }
-Console.WriteLine(args[0]);
 using var cts = new CancellationTokenSource();
-var token = (await JsonFileReader.ReadAsync<JsonFileReader.TokenFile>("token.json"))?.Token;
-if (token != null)
+var bot = new TelegramBotClient(token, cancellationToken: cts.Token);
+var user = await bot.GetMeAsync();
+bot.OnMessage += async (message, type) =>
 {
-    var bot = new TelegramBotClient(token, cancellationToken: cts.Token);
-    var user = await bot.GetMeAsync();
-    bot.OnMessage += async (message, type) =>
+    try
     {
-        try
-        {
-            if (message.Text is null) return;
-            Console.WriteLine($"Received {type} '{message.Text}' in {message.Chat}");
-            await bot.SendTextMessageAsync(message.Chat, $"{message.From} said: {message.Text}");
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine(ex.Message);
-        }
-    };
-    Console.WriteLine($"@{user.Username} запущен... Нажмите любую клавишу для выхода.");
-    Console.ReadLine();
-    cts.Cancel();
+        if (message.Text is null) return;
+        Console.WriteLine($"Received {type} '{message.Text}' in {message.Chat}");
+        await bot.SendTextMessageAsync(message.Chat, $"{message.From} said: {message.Text}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+};
+Console.WriteLine($"@{user.Username} запущен... Нажмите любую клавишу для выхода.");
+Console.ReadLine();
+cts.Cancel();
+return;
+
+string? TokenFileLoad()
+{
+    if (args.Length > 0 && File.Exists(args[0]))
+    {
+        return TokenFileReader.Read(args[0]);
+    }
+    return File.Exists("token.json") ? TokenFileReader.Read("token.json") : null;
 }
+
---
+++ TgBot_YT2Audio/TokenFileReader.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+
+namespace TgBot_YT2Audio;
+
+public static class TokenFileReader
+{
+    public static string? Read(string filePath)
+    {
+        try
+        {
+            using var stream = File.OpenRead(filePath);
+            var token = JsonSerializer.Deserialize<TokenFile>(stream);
+            return token?.Token;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            return null;
+        }
+    }
+
+    public class TokenFile
+    {
+        public string? Token { get; set; }
+    }
+}
